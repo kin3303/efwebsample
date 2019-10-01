a8d1ddc42894f4a2e60e2a03ec5e927691cc9a8d
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-10-01 13:18:34 +0900

    Update test.sql

--- univers/univers-web/src/main/webapp/DB/data/test.sql
+++ univers/univers-web/src/main/webapp/DB/data/test.sql
@@ -1,5 +1,29 @@
 USE `univers`;
 
+DROP TABLE IF EXISTS `university`;
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `university` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `university_city` varchar(255) DEFAULT NULL,
+  `university_country` varchar(255) DEFAULT NULL,
+  `university_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `age` int(11) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `university_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_a9nmvxoolj4das3ghh758un56` (`university_id`),
+  CONSTRAINT `FK_a9nmvxoolj4das3ghh758un56` FOREIGN KEY (`university_id`) REFERENCES `university` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 LOCK TABLES `university` WRITE;
 INSERT INTO `university` VALUES (1,'seoul','korea','snu'),(2,'chonbuk','korea','cbk'),(3,'seoul','korea','snut'),(4,'seoul','korea','cau'),(5,'seoul','korea','hufs'),(6,'seoul','korea','kku'),(7,'seoul','korea','hanyang');
 UNLOCK TABLES;
