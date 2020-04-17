df0597f260b1bf55960a0d54b3b9467d31bef5d4
kin3303 <48729027+kin3303@users.noreply.github.com>
2020-04-17 11:15:21 +0900

    Update UniversMainUI.java

--- univers/univers-ui/src/main/java/com/demo/ui/commons/UniversMainUI.java
+++ univers/univers-ui/src/main/java/com/demo/ui/commons/UniversMainUI.java
@@ -18,7 +18,7 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 @SpringUI(path=UniversMainUI.NAME)
-@Title("Univers-V2.5")
+@Title("Univers-3.1")
 @Theme("valo")
 public class UniversMainUI extends UI {
 	public static final String NAME="/ui";
